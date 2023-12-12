eb639c1d35e5a33b772d2b423179157d27bbe8af
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-12 16:05:14 +0100

    Update ingress configuration and install script

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -9,14 +9,18 @@ sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sudo kubectl wait --for=condition=Ready --timeout=300s -n argocd --all pod
 
+echo "installing ingress"
+sudo kubectl apply -f ../confs/ingress.yaml -n argocd
+echo "installed ingress, waiting..."
+sleep 3
+
 # Create Argo project
 sudo kubectl create namespace dev
 sudo kubectl apply -f ../confs/project.yaml -n argocd
-#sudo kubectl apply -f ../confs/ingress.yaml -n argocd
 sudo kubectl apply -f ../confs/application.yaml -n dev
 
 sudo kubectl wait --for=condition=Ready --timeout=300s -n dev --all pod
-sudo kubectl port-forward service/vchevill-playground -n dev 8888:8888 --address 0.0.0.0 & #& to run in background
+#sudo kubectl port-forward service/vchevill-playground -n dev 8888:8888 --address 0.0.0.0 & #& to run in background
 
 # Get Argo CD password
 echo "Argo CD credentials :"
@@ -26,8 +30,11 @@ echo
 echo
 
 # Access Argo CD
-sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0 & #& to run in background
+#sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0 & #& to run in background
 
 # Access Argo CD UI
 echo "Argo CD UI :"
 echo "https://localhost:8090"
+
+
+sudo kubectl get ingress -n argocd
