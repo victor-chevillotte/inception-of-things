b986fa73b52491d5f5ebd548d38ffdf8e54a7109
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-12 14:31:42 +0100

    Update application and project names

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-sudo k3d cluster create iot-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2 --wait
+# Create cluster
+sudo k3d cluster create iot-cluster -p 8080:80@loadbalancer -p 8443:443@loadbalancer
+sleep 5
 
+# Install Argo CD
 sudo kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl wait --for=condition=Ready --timeout=300s -n argocd --all pod
+
+# Create Argo project
 sudo kubectl create namespace dev
+sudo kubectl apply -f ../confs/project.yaml -n dev
+sudo kubectl apply -f ../confs/application.yaml -n dev
+
+echo "==============="
+sudo kubectl wait --for=condition=Ready --timeout=300s -n dev --all pod
+echo "Port forward"
+sudo kubectl port-forward service/will-playground -n dev 8888:8888 --address 0.0.0.0
+
+
+# Get Argo CD password
+echo "Argo CD credentials :"
+echo "admin"
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 2> /dev/null | base64 -d
+echo
+echo
+
+# Access Argo CD
+sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0
+
+# Access Argo CD UI
+echo "Argo CD UI :"
+echo "https://localhost:8090"
