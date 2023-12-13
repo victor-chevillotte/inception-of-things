db3950132101d777a138820680c3f368e236106b
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 22:08:01 +0100

    Update Gitlab port forwarding

--- bonus/scripts/install2.sh
+++ bonus/scripts/install2.sh
@@ -13,7 +13,7 @@ sudo helm search repo gitlab
 echo "[INFO]   Installing Gitlab, this may take a while..."
 sudo helm install gitlab gitlab/gitlab  --set global.hosts.https="false" --set global.ingress.configureCertmanager="false" --set gitlab-runner.install="false" -n gitlab
 #--set global.hosts.domain=$DOMAIN --set certmanager-issuer.email=$EMAIL
-echo "[INFO]   Gitlab URL: localhost:8181"
+echo "[INFO]   Gitlab URL: localhost:8085"
 echo "[INFO]   Gitlab username: root"
 echo "[INFO]   Gitlab password: "
 
@@ -22,4 +22,4 @@ sudo kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpat
 echo 'Waiting for gitlab to be deployed'
 sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
 
-sudo kubectl port-forward --address 0.0.0.0 service/gitlab-webservice-default -n gitlab 8181:8181 
+sudo kubectl port-forward --address 0.0.0.0 service/gitlab-webservice-default -n gitlab 8085:8181 
