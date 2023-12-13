ff79e747bf5708b9d6e0ffa64587896d33692005
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 15:29:26 +0100

    Remove ingress configuration and update installation script

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 # Create cluster
-sudo k3d cluster create iot-cluster -p 8090:80@loadbalancer -p 8443:443@loadbalancer
-sleep 5
+sudo k3d cluster create iot-cluster  -p 8443:443@loadbalancer -p 8888:8888@loadbalancer 
 
 # Install Argo CD
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sudo kubectl wait --for=condition=Ready --timeout=300s -n argocd --all pod
 
-echo "installing ingress"
-export KUBECONFIG="$(sudo k3d kubeconfig write iot-cluster)"
-sudo kubectl create deployment nginx --image=nginx -n argocd
-sudo kubectl create service clusterip nginx --tcp=80:80 -n argocd
-sudo kubectl apply -f ../confs/ingress.yaml -n argocd
-echo "installed ingress, waiting..."
-sleep 3
+sudo kubectl rollout status deployment argocd-server -n argocd
+sudo kubectl rollout status deployment argocd-redis -n argocd
+sudo kubectl rollout status deployment argocd-repo -n argocd
+sudo kubectl rollout status deployment argocd-dex-server -n argocd
+
+#sudo kubectl wait --for=condition=Ready --timeout=300s -n argocd --all pod
 
 # Create Argo project
 sudo kubectl create namespace dev
 sudo kubectl apply -f ../confs/project.yaml -n argocd
-sudo kubectl apply -f ../confs/application.yaml -n dev
+sudo kubectl apply -f ../confs/application.yaml -n argocd
 
 sudo kubectl wait --for=condition=Ready --timeout=300s -n dev --all pod
-#sudo kubectl port-forward service/vchevill-playground -n dev 8888:8888 --address 0.0.0.0 & #& to run in background
 
 # Get Argo CD password
 echo "Argo CD credentials :"
@@ -33,11 +29,8 @@ echo
 echo
 
 # Access Argo CD
-#sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0 & #& to run in background
+sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0 & #& to run in background
 
 # Access Argo CD UI
 echo "Argo CD UI :"
 echo "https://localhost:8090"
-
-
-sudo kubectl get ingress -n argocd
