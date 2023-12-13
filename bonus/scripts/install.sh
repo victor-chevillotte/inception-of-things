2d5762fa88a692737c91095ba1ae2d78446c5070
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 16:51:57 +0100

    Delete Vagrantfile and install_in_vm.sh

---
+++ bonus/scripts/install.sh
@@ -0,0 +1,29 @@
+# install Helm
+sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# create namespace for gitlab
+sudo kubectl create namespace gitlab
+
+echo "Adding Gitlab Helm repository..."
+sudo helm repo add gitlab https://charts.gitlab.io/
+sudo helm repo update
+
+echo "Installing Gitlab, this may take a while..."
+echo "to follow the deployment : sudo kubectl get all -n gitlab or helm status gitlab -n gitlab"
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  -n gitlab \
+  -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+  --set global.hosts.domain=10.11.1.253.nip.io \
+  --set global.hosts.externalIP=10.11.1.253 \
+  --set global.edition=ce \
+  --timeout 600s
+
+
+sudo kubectl wait --for=condition=available deployments --all -n gitlab --timeout=1200s
+
+echo "Credentials :"
+echo "root"
+sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode ; echo
+
+echo "Gitlab is deployed !"
+sudo kubectl port-forward service/gitlab-webservice-default 8181:8181 -n gitlab &
