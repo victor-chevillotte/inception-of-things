68ca078fc5bc0ce9669ee4217097e3f2222ae95b
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 22:07:42 +0100

    Update Gitlab installation configuration

--- bonus/scripts/install2.sh
+++ bonus/scripts/install2.sh
@@ -11,8 +11,8 @@ sudo helm repo add gitlab https://charts.gitlab.io/
 sudo helm search repo gitlab
 
 echo "[INFO]   Installing Gitlab, this may take a while..."
-sudo helm install gitlab gitlab/gitlab --set global.hosts.domain=$DOMAIN --set certmanager-issuer.email=$EMAIL --set global.hosts.https="false" --set global.ingress.configureCertmanager="false" --set gitlab-runner.install="false" -n gitlab
-
+sudo helm install gitlab gitlab/gitlab  --set global.hosts.https="false" --set global.ingress.configureCertmanager="false" --set gitlab-runner.install="false" -n gitlab
+#--set global.hosts.domain=$DOMAIN --set certmanager-issuer.email=$EMAIL
 echo "[INFO]   Gitlab URL: localhost:8181"
 echo "[INFO]   Gitlab username: root"
 echo "[INFO]   Gitlab password: "
@@ -22,4 +22,4 @@ sudo kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpat
 echo 'Waiting for gitlab to be deployed'
 sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
 
-sudo kubectl port-forward --address 0.0.0.0 service/gitlab-webservice-default -n gitlab 8085:8181 | kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443 
+sudo kubectl port-forward --address 0.0.0.0 service/gitlab-webservice-default -n gitlab 8181:8181 
