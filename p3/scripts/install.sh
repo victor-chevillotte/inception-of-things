7469e1f1cd6f87aeb1dca624171f2342b7abbb96
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 17:19:00 +0100

    Working whithout port forward !

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,36 +1,41 @@
-#!/bin/bash
+echo "Creating cluster"
+sudo k3d cluster create iot-cluster -p 8080:80@loadbalancer -p 8888:30888@loadbalancer
+echo "Cluster created, waiting..."
 
-# Create cluster
-sudo k3d cluster create iot-cluster  -p 8443:443@loadbalancer -p 8888:8888@loadbalancer 
+sleep 5
 
-# Install Argo CD
+echo "Creating namespaces"
 sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-sudo kubectl rollout status deployment argocd-server -n argocd
-sudo kubectl rollout status deployment argocd-redis -n argocd
-sudo kubectl rollout status deployment argocd-repo -n argocd
-sudo kubectl rollout status deployment argocd-dex-server -n argocd
-
-#sudo kubectl wait --for=condition=Ready --timeout=300s -n argocd --all pod
-
-# Create Argo project
 sudo kubectl create namespace dev
+echo "Namespaces created"
+
+echo "Installing argocd"
+sudo kubectl apply -n argocd -f ../confs/install.yaml
+echo "Argo CD installed, waiting..."
+
+echo "Waiting the pods to be ready"
+sudo kubectl wait -n argocd --for=condition=Ready pods --all
+echo "Pods ready, waiting..."
+
+echo "Installing ingress"
+sudo kubectl apply -f ../confs/ingress.yaml -n argocd
+echo "Installed ingress, waiting..."
+
+
+echo "Installing project to argocd"
 sudo kubectl apply -f ../confs/project.yaml -n argocd
+echo "Installed project to argocd"
+
+echo "Installing application to argocd"
 sudo kubectl apply -f ../confs/application.yaml -n argocd
+echo "application installed  to argocd"
 
-sudo kubectl wait --for=condition=Ready --timeout=300s -n dev --all pod
+echo "Everything is installed!"
 
-# Get Argo CD password
 echo "Argo CD credentials :"
 echo "admin"
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 2> /dev/null | base64 -d
-echo
-echo
 
-# Access Argo CD
-sudo kubectl port-forward service/argocd-server -n argocd 8090:443 --address 0.0.0.0 & #& to run in background
-
-# Access Argo CD UI
+echo
 echo "Argo CD UI :"
-echo "https://localhost:8090"
+echo "http://localhost:8080"
