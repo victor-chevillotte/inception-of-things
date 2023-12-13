606c43c5969314a2c6dde283ae41c6af563799f8
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-14 00:26:01 +0100

    Update Gitlab installation script

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -15,13 +15,15 @@ sudo helm search repo gitlab
 echo "[INFO]   Installing Gitlab, this may take a while..."
 sudo helm install gitlab gitlab/gitlab   --set global.hosts.https="false" --set global.ingress.configureCertmanager="false" --set gitlab-runner.install="false" -n gitlab
 
+echo 'Gitlab installed ! Waiting for gitlab to be deployed'
+sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
+
+
 echo "[INFO]   Gitlab URL: http://localhost:8085"
 echo "[INFO]   Gitlab username: root"
 echo "[INFO]   Gitlab password: "
 
 sudo kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
 
-echo 'Waiting for gitlab to be deployed'
-sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
 
 #sudo kubectl port-forward --address 0.0.0.0 service/gitlab-webservice-default -n gitlab 8085:8181 
