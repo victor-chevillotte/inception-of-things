606e89fff1bc829e1767b032c7db999238d99795
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-13 17:28:54 +0100

    P3 finished !!

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,5 +1,6 @@
 echo "Creating cluster"
-sudo k3d cluster create iot-cluster  -p 8443:443@loadbalancer -p 8888:8888@loadbalancer 
+sudo k3d cluster create iot-cluster -p 8080:80@loadbalancer  -p 8888:8888@loadbalancer --agents 2
+
 echo "Cluster created, waiting..."
 
 sleep 5
@@ -7,14 +8,14 @@ sleep 5
 echo "Creating namespaces"
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
-echo "Namespaces created"
+echo "Namespaces created, waiting..."
 
 echo "Installing argocd"
 sudo kubectl apply -n argocd -f ../confs/install.yaml
 echo "Argo CD installed, waiting..."
 
-echo "Waiting the pods to be ready"
-sudo kubectl wait -n argocd --for=condition=Ready pods --all
+echo "Waiting for pods to be ready"
+sudo kubectl wait -n argocd --for=condition=Ready pods --all --timeout=600s
 echo "Pods ready, waiting..."
 
 echo "Installing ingress"
@@ -29,13 +30,11 @@ echo "Installed project to argocd"
 echo "Installing application to argocd"
 sudo kubectl apply -f ../confs/application.yaml -n argocd
 echo "application installed  to argocd"
-
+echo "\n\n\n"
 echo "Everything is installed!"
 
-echo "Argo CD credentials :"
-echo "admin"
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 2> /dev/null | base64 -d
-
-echo
-echo "Argo CD UI :"
+echo "Argo CD access :"
 echo "http://localhost:8080"
+echo "username :admin"
+echo "password :"
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 2> /dev/null | base64 -d
